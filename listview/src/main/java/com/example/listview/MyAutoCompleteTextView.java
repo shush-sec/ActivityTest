dad956be7b3384c1473708158514656952e814ea
shush.sec <shush.sec@gmail.com>
2017-05-12 15:19:04 +0800

    t

--- listview/src/main/java/com/example/listview/MyAutoCompleteTextView.java
+++ listview/src/main/java/com/example/listview/MyAutoCompleteTextView.java
@@ -2,6 +2,8 @@ package com.example.listview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.SimpleAdapter;
 
@@ -34,22 +36,23 @@ public class MyAutoCompleteTextView extends AppCompatActivity {
 //
 //    }
 
+    //自定义autoCompleteTextview
     public void loadMyAutoCompleteTextView() {
-        AutoCompleteTextView autoCompleteTextView =
+        final AutoCompleteTextView autoCompleteTextView =
                 (AutoCompleteTextView) findViewById(R.id.autoCompleteTv);
 
         Map<String, Object> map1 = new HashMap<>();
-        map1.put("text_name", "beijing");
         map1.put("imageView_logo",String.valueOf(R.mipmap.ic_launcher) );
+        map1.put("text_name", "beijing");
 
         Map<String, Object> map2 = new HashMap<>();
-        map2.put("text_name", "beiping");
         map2.put("imageView_logo", String.valueOf(R.mipmap.ic_launcher_round));
+        map2.put("text_name", "beiping");
 
 
         Map<String, Object> map3 = new HashMap<>();
-        map3.put("text_name", "guangzhou");
         map3.put("imageView_logo", String.valueOf(R.mipmap.ic_launcher));
+        map3.put("text_name", "guangzhou");
 
         List<Map<String, Object>> list = new ArrayList<>();
         list.add(map1);
@@ -62,6 +65,19 @@ public class MyAutoCompleteTextView extends AppCompatActivity {
                 , new String[]{"imageView_logo", "text_name"}
                 , new int[]{R.id.imageView_logo, R.id.text_name});
         autoCompleteTextView.setAdapter(adapter);
+
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Map<String, Object> map = new HashMap<>();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                map = (HashMap)parent.getItemAtPosition(position);
+                String name =map.get("text_name").toString();
+
+                autoCompleteTextView.setText(name);
+            }
+        });
 
     }
 }
