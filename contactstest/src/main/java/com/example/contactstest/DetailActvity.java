5127075b75af8886f3644c905bcb2da06a43ae69
shush.sec <shush.sec@gmail.com>
2017-05-19 19:58:11 +0800

    获取电话联系人手机号和email

---
+++ contactstest/src/main/java/com/example/contactstest/DetailActvity.java
@@ -0,0 +1,71 @@
+package com.example.contactstest;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        List<String> numberList = new ArrayList<>();
+        List<String> emailsList = new ArrayList<>();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_actvity);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+
+        //获取某联系人电话号码列表
+        String number ;
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id,
+                null, null);
+
+        if (phones.getCount() > 0) {
+             while (phones.moveToNext()){
+                number = phones
+                        .getString(phones.getColumnIndex
+                                (ContactsContract.CommonDataKinds.Phone.NUMBER));
+                numberList.add(number);
+            }
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this
+                , android.R.layout.simple_list_item_1
+                , numberList);
+
+        ListView listView = (ListView) findViewById(R.id.list_number);
+        listView.setAdapter(adapter);
+
+        //获取某人的email
+        String email;
+        Cursor emails = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id,
+                null, null);
+        if (emails.getCount() > 0) {
+            while (emails.moveToNext()){
+                email = emails
+                        .getString(emails.getColumnIndex
+                                (ContactsContract.CommonDataKinds.Email.DATA));
+                emailsList.add(email);
+            }
+        }
+
+        ArrayAdapter<String> emailAdapter = new ArrayAdapter<>(this
+                , android.R.layout.simple_list_item_1
+                , emailsList);
+
+        ListView listViewEmail = (ListView) findViewById(R.id.list_email);
+        listViewEmail.setAdapter(emailAdapter);
+    }
+}
--- contactstest/src/main/java/com/example/contactstest/MainActivity.java
+++ contactstest/src/main/java/com/example/contactstest/MainActivity.java
@@ -1,15 +1,66 @@
 package com.example.contactstest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
+    List<String> contactList = new ArrayList<>();
+    Map<String,String> contactMap = new HashMap<>() ;  //这个map用于传递contactId到DetailActivity
+    String contactName = null;
+    String contactId = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Cursor cursor = getContentResolver()
+                .query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null, null);
+
+        //获取联系人名列表
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                contactName = cursor
+                        .getString(cursor.getColumnIndex
+                                (ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+               contactId= cursor
+                        .getString(cursor.getColumnIndex
+                                (ContactsContract.Contacts._ID));
+                contactList.add(contactName);
+                contactMap.put(contactName,contactId);
+            }
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this
+                , android.R.layout.simple_list_item_1
+                , contactList);
+
+        ListView listView = (ListView) findViewById(R.id.contacts_view);
+        listView.setAdapter(adapter);
+             //监听listView点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,DetailActvity.class);
+                String contactID =  contactMap.get( contactList.get(position))  ;
+                intent.putExtra("id", contactID);
+                startActivity(intent);
+            }
+        });
     }
+
 }
