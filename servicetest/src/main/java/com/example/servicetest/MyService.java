f94df575a11ce94f7a4242f852a63593bfbe969a
shush.sec <shush.sec@gmail.com>
2017-05-19 09:52:56 +0800

    添加监听电话

--- servicetest/src/main/java/com/example/servicetest/MyService.java
+++ servicetest/src/main/java/com/example/servicetest/MyService.java
@@ -1,15 +1,43 @@
 package com.example.servicetest;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class MyService extends Service {
 
     public static final String TAG = "MyService";
+
+    private TelephonyManager tm;                 //电话管理器
+    private MyPhoneStateListener listener;      //监听器
+
+    //接听电话状态
+    private class MyPhoneStateListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    Log.d(TAG, "电话状态:闲置 ");
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Log.d(TAG, "电话状态:响铃");
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Log.d(TAG, "电话状态:接听");
+                    break;
+            }
+        }
+    }
+
+
     private DownloadBinder mBinder = new DownloadBinder();
+
     class DownloadBinder extends Binder {
         public void startDownload() {
             Log.d(TAG, "startDownload: ");
@@ -28,6 +56,10 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate: ");
+        //开启监听
+        tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        listener = new MyPhoneStateListener();
+        tm.listen(listener,PhoneStateListener.LISTEN_CALL_STATE);
     }
 
     @Override
@@ -35,11 +67,12 @@ public class MyService extends Service {
         Log.d(TAG, "onStartCommand: ");
         return super.onStartCommand(intent, flags, startId);
     }
-    
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+        //tm.listen(listener,PhoneStateListener.LISTEN_CALL_STATE);
     }
 
     @Override
